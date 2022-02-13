79fc4ba132f7ea0045f243439f02fd1e275e1949
Tod Kurt <todbot@users.noreply.github.com>
2022-02-12 17:32:28 -0800

    appease linter mostly

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,66 +1,66 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import HID from 'node-hid';
 
-import HID from 'node-hid'
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+// import icon from '../../assets/icon.svg';
+import './App.css';
 
 const devices = HID.devices();
 
-devices.sort((a,b)=> b.vendorId-a.vendorId || b.productId-a.productId )
+devices.sort((a, b) => b.vendorId - a.vendorId || b.productId - a.productId);
 
+/* eslint-disable */
 function decimalToHex(decimal, chars) {
-    return (decimal + Math.pow(16, chars)).toString(16).slice(-chars).toUpperCase();
+  return (decimal + Math.pow(16, chars)).toString(16).slice(-chars).toUpperCase();
 }
+/* eslint-enable */
 
 const Hello = () => {
-    var createDeviceLine = function(device,index) {
-        return (
-            <tr key={index}>
-                <td>0x{decimalToHex(device.vendorId, 4)}</td>
-                <td>0x{decimalToHex(device.productId, 4)}</td>
-                <td>0x{decimalToHex(device.usagePage, 4)}</td>
-                <td>0x{decimalToHex(device.usage, 4)}</td>
-                <td>{device.manufacturer}</td>
-                <td>{device.product}</td>
-                <td>{device.serialNumber}</td>
-                <td>{device.path}</td>
-            </tr>
-        );
-    }
+  const createDeviceLine = (device, index) => {
+    return (
+      <tr key={index}>
+        <td>0x{decimalToHex(device.vendorId, 4)}</td>
+        <td>0x{decimalToHex(device.productId, 4)}</td>
+        <td>0x{decimalToHex(device.usagePage, 4)}</td>
+        <td>0x{decimalToHex(device.usage, 4)}</td>
+        <td>{device.manufacturer}</td>
+        <td>{device.product}</td>
+        <td>{device.serialNumber}</td>
+        <td>{device.path}</td>
+      </tr>
+    );
+  };
 
   return (
     <div>
-
       <h1>Hello from electron-hid-test-erb</h1>
 
       <div>
-      We are using node { process.versions.node },
-      Chrome {process.versions.chrome},
-      and Electron { process.versions.electron }.
+        We are using NodeJs {process.versions.node},
+        Chrome {process.versions.chrome},
+        and Electron {process.versions.electron}.
       </div>
 
       <h2> HID Device List </h2>
 
       <table>
-      <thead>
-      <tr><th> vendorId </th>
-          <th> productId </th>
-          <th> usagePage </th>
-          <th> usage </th>
-          <th> manufacturer </th>
-          <th> product </th>
-          <th> serialNumber </th>
-          <th> path </th>
-      </tr>
-      </thead>
-      <tbody>
-      {devices.map(createDeviceLine, this)}
-      </tbody>
+        <thead>
+          <tr>
+            <th>vendorId</th>
+            <th>productId</th>
+            <th>usagePage</th>
+            <th>usage</th>
+            <th>manufacturer</th>
+            <th>product</th>
+            <th>serialNumber</th>
+            <th>path</th>
+          </tr>
+        </thead>
+        <tbody>{devices.map(createDeviceLine, this)}</tbody>
       </table>
 
       <h2> HID Device List (JSON) </h2>
       <pre>
-      { JSON.stringify(HID.devices(),null, '  ') }
+      {JSON.stringify(HID.devices(), null, '  ')}
       </pre>
     </div>
   );
